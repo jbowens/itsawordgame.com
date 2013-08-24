d314479e94c6169410790b7ecfd4c0d913a2d3b6
Jackson Owens <jackson_owens@brown.edu>
2013-08-24 13:46:18 -0700

    changed hover capture radius for letters

--- game.js
+++ game.js
@@ -8,49 +8,48 @@
         // TODO: Do setup
     });
 
-    var width = 6;
-    var height = 5;
+    var width = 5;
+    var height = 4;
 
     function constructCell(letter, row, column) {
         var cell = $('<div class="cell"></div>');
         cell.data('row', row);
         cell.data('column', column);
 
-        var cellOverlay = $('<div class="overlay"></div>');
-        cell.append(cellOverlay);
-
         var innerCell = $('<div class="inner-cell"></div>');
-        innerCell.text(letter);
+        var letterSpan = $('<span class="letter"></span>');
+        letterSpan.text(letter);
+        innerCell.append(letterSpan);
         cell.append(innerCell);
-        cell.hover(cellHover, cellExit);
+        letterSpan.hover(cellHover, cellExit);
         return cell;
     }
 
     function cellHover(e) {
-        $(this).addClass('hover');
+        var cell = $(this).closest('.cell');
+        cell.addClass('hover');
     }
 
     function cellExit(e) {
-        var cell = $(this);
-        var overlay = cell.find('.overlay');
+        var cell = $(this).closest('.cell');
+        var innerCell = cell.find('.inner-cell');
         cell.removeClass('hover');
-        // Do an exit fade animation
 
+        // Do an exit fade animation
         if (cell.data('animationTimeout')) {
             clearTimeout(cell.data('animationTimeout'));
             cell.data('animationTimeout', null);
         }
 
-        var num = 20;
+        var num = 30;
         function animationTick() {
-            var alpha = num / 20.0;
+            var alpha = num / 30.0;
             var color = 'rgba(163,67,99,'+alpha+')';
-            console.log(overlay);
-            overlay.css('background-color', color);
+            innerCell.css('background-color', color);
             num--;
 
             if (num > 0)
-                cell.data('animationTimeout', setTimeout(animationTick, 50));
+                cell.data('animationTimeout', setTimeout(animationTick, 30));
         }
 
         animationTick();
@@ -59,6 +58,7 @@
 
     function constructRandomBoard() {
         var board = $("#board");
+        var clearfix = $("#board .clearfix");
 
         for (var r = 0; r < height; r++)
         {
@@ -67,7 +67,7 @@
                 var randomNumber = Math.floor(Math.random() * 26);
                 var randomLetter = String.fromCharCode(randomNumber + 65);
                 var cell = constructCell(randomLetter, r, c);
-                board.append(cell);
+               $(cell).insertBefore(clearfix); 
             }
         }
     }

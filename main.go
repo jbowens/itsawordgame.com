45d0ab0a96846ee7a6f1603bfafd6602cae7f1c2
Jackson Owens <jackson_owens@alumni.brown.edu>
2015-11-29 12:27:58 -0800

    Clean up main()

--- main.go
+++ main.go
@@ -1,17 +1,8 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/jbowens/itsawordgame.com/app"
-	"github.com/jbowens/itsawordgame.com/app/game"
-)
+import "github.com/jbowens/itsawordgame.com/app"
 
 func main() {
-
-	b := game.NewBoard(5, 5)
-	fmt.Println(b.String())
-
 	var a app.App
 	a.Start()
 	a.Wait()
